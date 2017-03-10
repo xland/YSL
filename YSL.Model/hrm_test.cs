d8a56c75983a0570f3f5736e028a9e0a96219ac7
liulun <412588801@qq.com>
2017-03-10 14:33:13 +0800

    考试

---
+++ YSL.Model/hrm_test.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSL.Model
+{
+    /// <summary>
+    /// 考试
+    /// </summary>
+    public class hrm_test
+    {
+        /// <summary>
+        /// 考试ID
+        /// </summary>
+        public string id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 考试名称
+        /// </summary>
+        public string test_name
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 考试创建时间
+        /// </summary>
+        public DateTime add_time
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 创建考试的账户ID
+        /// </summary>
+        public string add_account_id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 考试开始时间
+        /// </summary>
+        public DateTime test_begin_time
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 考试结束时间
+        /// </summary>
+        public DateTime test_end_time
+        {
+            get;
+            set;
+        }
+    }
+}
