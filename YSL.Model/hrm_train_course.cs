5e9512b152fe62412e0338cb1515508b4fe9c909
liulun <412588801@qq.com>
2017-03-11 14:56:57 +0800

    会议室、课程计划

---
+++ YSL.Model/hrm_train_course.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSL.Model
+{
+    /// <summary>
+    /// 课程计划
+    /// </summary>
+    public class hrm_train_course
+    {
+        /// <summary>
+        /// 课程计划ID
+        /// </summary>
+        public string id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 课程计划开始时间
+        /// </summary>
+        public DateTime begin_time
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 课程计划结束时间
+        /// </summary>
+        public DateTime end_time
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 课程计划描述
+        /// </summary>
+        public string des
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 内部课程会议室ID
+        /// </summary>
+        public string meeting_room_id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 是否是外部课程
+        /// </summary>
+        public int is_external
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 外部课程省ID
+        /// </summary>
+        public string province_id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 外部课程市ID
+        /// </summary>
+        public string city_id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 外部课程县ID
+        /// </summary>
+        public string area_id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 外部课程的具体地址
+        /// </summary>
+        public string address
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 课程是否已完成：0已完成；
+        /// </summary>
+        public int is_end
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ YSL.Model/mm_meeting_room.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSL.Model
+{
+    /// <summary>
+    /// 会议室
+    /// </summary>
+    public class mm_meeting_room
+    {
+        /// <summary>
+        /// 会议室编号
+        /// </summary>
+        public string id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 会议室名称
+        /// </summary>
+        public string room_name
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 会议室描述
+        /// </summary>
+        public string des
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 会议室地址
+        /// </summary>
+        public string room_address
+        {
+            get;
+            set;
+        }
+    }
+}
