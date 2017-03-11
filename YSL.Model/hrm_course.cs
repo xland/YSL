14771ef5d9930f137b4c22cd3332f98ae2001a80
liulun <412588801@qq.com>
2017-03-11 15:08:02 +0800

    课程、课程讲师

---
+++ YSL.Model/hrm_course.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSL.Model
+{
+    /// <summary>
+    /// 课程
+    /// </summary>
+    public class hrm_course
+    {
+        /// <summary>
+        /// 课程ID
+        /// </summary>
+        public string id
+        {
+            get;
+            set;            
+        }
+        /// <summary>
+        /// 课程名称
+        /// </summary>
+        public string course_name
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 课程描述
+        /// </summary>
+        public string des
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 课程难度系数
+        /// </summary>
+        public int level
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ YSL.Model/hrm_course_teacher.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSL.Model
+{
+    /// <summary>
+    /// 课程讲师
+    /// </summary>
+    public class hrm_course_teacher
+    {
+        /// <summary>
+        /// 讲师ID
+        /// </summary>
+        public string id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 内部讲师员工ID
+        /// </summary>
+        public string employee_id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 内部讲师帐号ID
+        /// </summary>
+        public string account_id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 是否内部员工
+        /// </summary>
+        public string is_external
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 外部讲师名称
+        /// </summary>
+        public string teacher_name
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 课程ID
+        /// </summary>
+        public string course_id
+        {
+            get;
+            set;
+        }
+    }
+}
