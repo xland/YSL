b8f343bd972d1e5656fa0f2b5f1c41674fde597d
liulun <412588801@qq.com>
2017-03-11 15:16:01 +0800

    课程文档

---
+++ YSL.Model/hrm_course_doc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSL.Model
+{
+    /// <summary>
+    /// 课程文档
+    /// </summary>
+    public class hrm_course_doc
+    {
+        /// <summary>
+        /// 文档ID
+        /// </summary>
+        public string id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 课程ID
+        /// </summary>
+        public string course_id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 文档路径
+        /// </summary>
+        public string doc_url
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 文档描述
+        /// </summary>
+        public string des
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 文档排序
+        /// </summary>
+        public int order_num
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 是否已过期 
+        /// </summary>
+        public int is_out_date
+        {
+            get;
+            set;
+        }
+    }
+}
