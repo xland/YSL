9faadd140c980790efff63967f30fdec76c4bf7a
liulun <412588801@qq.com>
2017-03-11 15:26:35 +0800

    受训人员

---
+++ YSL.Model/hrm_train_course_employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YSL.Model
+{
+    /// <summary>
+    /// 受训人员
+    /// </summary>
+    public class hrm_train_course_employee
+    {
+        /// <summary>
+        /// 受训人员ID
+        /// </summary>
+        public string id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 员工ID
+        /// </summary>
+        public string employee_id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 是否必培
+        /// </summary>
+        public int is_have_to
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// 培训课程计划ID
+        /// </summary>
+        public string hrm_train_course_id
+        {
+            get;
+            set;
+        }
+    }
+}
